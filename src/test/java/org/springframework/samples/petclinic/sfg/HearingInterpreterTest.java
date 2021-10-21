89572790a0bebcc006912ffadf222a09e1cd2643
boris.stojanovic <boris.stojanovic@msg-global.com>
2021-10-21 09:52:52 +0200

    Setup unit test with autowiring

---
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {BaseConfig.class, LaurelConfig.class})
+class HearingInterpreterTest {
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+
+        assertEquals("Laurel", word);
+    }
+}
