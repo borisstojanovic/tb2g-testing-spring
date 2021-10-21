7eef1e66832700eed784a9290f7e99763193cfa5
boris.stojanovic <boris.stojanovic@msg-global.com>
2021-10-21 10:07:07 +0200

    Added inner class config test

--- src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/HearingInterpreterYannyTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = {BaseConfig.class, YannyConfig.class})
+//@SpringJUnitConfig(classes = {BaseConfig.class, YannyConfig.class}) different option to setup when using junit 5
 public class HearingInterpreterYannyTest {
     @Autowired
     HearingInterpreter hearingInterpreter;
